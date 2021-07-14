ecb5b69fba882d10bcbccacb077ee5722ae4e612
heathkang <heath.kang@foxmail.com>
2021-07-14 23:30:18 +0800

    update: 1. update akka stream in zio

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,28 +5,38 @@ import akka.actor.ActorSystem
 import org.heathkang.scala3_stream.mqttSource.mqttSource
 import akka.stream.alpakka.mqtt.MqttMessage
 import scala.concurrent.Future
-import zio.{ Has, Layer, Managed, Task, ZLayer }
+import zio._
 import com.github.ekeith.zio.akkastream.Converters.runnableGraphAsZioEffect
 import org.json4s
 import org.json4s.native.JsonMethods._
 import org.json4s.JValue
 
+object MyApp extends zio.App {
+  // given ActorSystem = ActorSystem("Stream-Start")
 
-@main def hello: Unit = 
-  given ActorSystem = ActorSystem("Stream-Start")
-  createSource.runForeach(i => println(i))
-  val runGraph: RunnableGraph[Future[Done]] = mqttSource.mqttSource.via(createFlow).toMat(toSink)(Keep.right)
-  runGraph.run()
+  def run(args: List[String]) = 
+    val runtime = Runtime.default
+  
+    val runGraph: RunnableGraph[Future[Done]] = mqttSource.mqttSource.via(createFlow).toMat(toSink)(Keep.right)
+    
+    (for {
+        materialisedValue <- runnableGraphAsZioEffect(runGraph).provideLayer(materializerLayer)
+    } yield materialisedValue).exitCode
+  // runGraph.run()
 
-def msg = "I was compiled by Scala 3. :)"
+  val actorSystem: Layer[Throwable, Has[ActorSystem]] = 
+    ZLayer.fromManaged(Managed.make(Task(ActorSystem("Stream-start")))
+      (sys => Task.fromFuture(_ => sys.terminate()).either))
 
-def createSource: Source[Int, NotUsed] = 
-  Source(1 to 10)
+  val materializerLayer: Layer[Throwable, Has[Materializer]] = 
+    actorSystem >>> ZLayer.fromFunction(as => Materializer(as.get))
 
-def createFlow: Flow[MqttMessage, JValue, NotUsed] = 
-  Flow[MqttMessage].map(
-    mqttMessage => parse(mqttMessage.payload.utf8String)
-  )
+  def createFlow: Flow[MqttMessage, JValue, NotUsed] = 
+    Flow[MqttMessage].map(
+      mqttMessage => parse(mqttMessage.payload.utf8String)
+    )
 
-def toSink: Sink[JValue, Future[Done]] =
-  Sink.foreach(s => println(compact(render(s))))
+  def toSink: Sink[JValue, Future[Done]] =
+    Sink.foreach(s => println(compact(render(s))))
+
+}
